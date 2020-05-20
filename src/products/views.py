cab8f373f2d5e0b236d18be24430de015279c8d6
YASIN-ERGB <ergaibi.yassine@gmail.com>
2020-05-20 03:46:04 +0000

    updating cart with jquery ajax

--- src/products/views.py
+++ src/products/views.py
@@ -19,9 +19,7 @@ class ProductFeaturedDetailView(DetailView):
     queryset = Product.objects.all().featured()        
     template_name="products/featured-detail.html"
 
-    # def get_queryset(self, *args, **kwargs):
-    #     request = self.request
-    #     return Product.objects.all().featured()
+   
     
 
 
@@ -29,10 +27,13 @@ class ProductListView(ListView):
     
     template_name="products/list.html"\
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
+    
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
