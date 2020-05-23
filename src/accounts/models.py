1817c5054b4229967c7af983af51acbc86f74070
YASIN-ERGB <ergaibi.yassine@gmail.com>
2020-05-23 03:21:30 +0000

    Form and Admin for our custom user

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -5,13 +5,14 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, is_active=True, is_admin=False, is_staff=False):
+    def create_user(self, email, full_name=None, password=None, is_active=True, is_admin=False, is_staff=False):
         if not email:
             raise ValueError("Users must have an email.")
         if not password:
             raise ValueError("Users must have a password.")
         user_obj = self.model(
-            email = self.normalize_email(email)
+            email = self.normalize_email(email),
+            full_name = full_name
         )
         user_obj.set_password(password)
         user_obj.active = is_active
@@ -21,18 +22,20 @@ class UserManager(BaseUserManager):
         return user_obj
     
 
-    def create_staffuser(self, email, password=None):
+    def create_staffuser(self, email, full_name=None, password=None):
         user = self.create_user(
             email,
+            full_name = full_name,
             password=password,
             is_staff= True
         )
         return user
 
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, full_name=None, password=None):
         user = self.create_user(
             email,
+            full_name = full_name,
             password=password,
             is_staff= True,
             is_admin= True
@@ -42,7 +45,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     email       = models.EmailField(max_length=255, unique=True)
-#     # # full_name   = models.CharField(max_length=255, blank=True, null=True)
+    full_name   = models.CharField(max_length=255, blank=True, null=True)
     active      = models.BooleanField(default=True) #can login
     staff       = models.BooleanField(default=False)
     admin       = models.BooleanField(default=False)
@@ -60,6 +63,8 @@ class User(AbstractBaseUser):
 
 
     def get_full_name(self):
+        if self.full_name:
+            return self.full_name
         return self.email
 
 
@@ -69,7 +74,7 @@ class User(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         return True
         
-    def has_module_perm(self, app_label):
+    def has_module_perms(self, app_label):
         return True
    
 
